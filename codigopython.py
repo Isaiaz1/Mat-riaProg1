44a6892021c7d1309e5f69ec563e5ffc1701f3ac
Isaiaz1 <59181631+Isaiaz1@users.noreply.github.com>
2024-05-22 09:22:22 -0300

    Update codigopython.py

--- codigopython.py
+++ codigopython.py
@@ -1,29 +1,31 @@
-##livros biblioteca
+#livros biblioteca
 livros = []
-desenhos = [] #ilustração 
+desenhos = []
 capa1 = "ilustrada" 
 capa2 = "sem ilustração"
 
 for i in range(3):
   livro = input("qual o nome do livro ? ")
-desenho = input("o livro possui ilustração ? ") 
-livros.append(livro)
+  livros.append(livro)  #linha no loop for
+  desenho = input("o livro possui ilustração ? ") 
 
-if desenho == "sim":
+  if desenho == "sim":
     print(capa1)
     desenhos.append("sim")
-elif desenho == "não":
+  elif desenho == "não":
     print(capa2)
     desenhos.append("não")
-else:
+  else:
     print("Respostas aceitas: sim ou não")
 
 
-
-
-# Imprime a lista de livros
 print("Livros:", livros)
 print("O livro é ilustrado? " ,desenhos)
+
+
+
+
+
 
 
 def verifica_livro(livro, livros):
@@ -34,6 +36,8 @@ def verifica_livro(livro, livros):
 livro = input("qual o nome do livro ? ")
 if verifica_livro(livro, livros):
     print("Este livro já existe na lista.")
+else:
+    print(" não existe na lista")
 else:
     print(" não existe na lista")
 
